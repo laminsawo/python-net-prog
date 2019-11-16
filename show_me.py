8fe3a816acef0a8cc7b3f601962fc475da793035
Lamin Sawo <laminsawojadama@outlook.com>
2019-11-16 19:25:18 +0000

    removed duplicated vars that resulted in connection to just on switch

--- show_me.py
+++ show_me.py
@@ -30,20 +30,30 @@ cisco_3560_32 = {
     'port' : 22,          # optional, defaults to 22
     'secret': secret_pass,     # optional, defaults to ''
 }
-net_connect = ConnectHandler(**cisco_3560_30)
-net_connect = ConnectHandler(**cisco_3560_31)
-net_connect = ConnectHandler(**cisco_3560_32)
+net_connect_1 = ConnectHandler(**cisco_3560_30)
+net_connect_2 = ConnectHandler(**cisco_3560_31)
+net_connect_3 = ConnectHandler(**cisco_3560_32)
 # established a connection to the object i.e cisco device
 
-output = net_connect.send_command('show ip int brief')
+output_int_1 = net_connect.send_command('show ip int brief')
 # sends command to show ip interface brief of interfaces
-
-output2 = net_connect.send_command('show cdp neighbors')
+output_cdp_1 = net_connect.send_command('show cdp neighbors')
 # displays CDP neighbors
 
-print(output + "\n" + output2)
-# prints the show command outputs and seperates them by a line
+output_int_2 = net_connect.send_command('show ip int brief')
+# sends command to show ip interface brief of interfaces
+output_cdp_2 = net_connect.send_command('show cdp neighbors')
+# displays CDP neighbors
 
+output_int_3 = net_connect.send_command('show ip int brief')
+# sends command to show ip interface brief of interfaces
+output_cdp_3 = net_connect.send_command('show cdp neighbors')
+# displays CDP neighbors
+
+print(output_int_1 + "\n" + output_int_2 + "\n" + output_int_3 )
+# prints the interface brief of interfaces
+print(output_cdp_1 + "\n" + output_cdp_2 + "\n" + output_cdp_3 )
+# prints the cdp neighbor relationship for each switch 
 
 net_connect.disconnect()
 # disconnects the connections
