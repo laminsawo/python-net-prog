e361730fc5f3cb155209754c5aee2e9c533eb004
laminsawo <laminsawojadama@outlook.com>
2019-11-16 17:25:35 +0000

    added a second show command to display CDP neighbors

--- show_me.py
+++ show_me.py
@@ -1,14 +1,14 @@
 from netmiko import ConnectHandler
-
 cisco_3560 = {
     'device_type': 'cisco_ios',
     'host':   '192.168.0.30',
-    'username': 'admin',
-    'password': 'Password01',
+    'username':"admin",
+    'password': "Password01",
     'port' : 22,          # optional, defaults to 22
-    'secret': 'cisco',     # optional, defaults to ''
+    'secret': "cisco",     # optional, defaults to ''
 }
 net_connect = ConnectHandler(**cisco_3560)
 
 output = net_connect.send_command('show ip int brief')
-print(output)
+output2 = net_connect.send_command('show cdp neighbors')
+print(output + "\n" + output2)
